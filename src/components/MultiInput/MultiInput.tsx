e4856be6e43a893c8ef6fc40cebee6b42d0ff3fa
yukio52hz <iamfarid1996@gmail.com>
2023-05-08 10:11:32 -0600

    multiInput changes

--- src/components/MultiInput/MultiInput.tsx
+++ src/components/MultiInput/MultiInput.tsx
@@ -27,11 +27,12 @@ export interface MultiInputProps {
     id?: string | undefined,
     name?: string | undefined,
     textInputRequired?:  ReactNode,
+    inputStyle?:SxProps<Theme> | undefined,
 }
 
 export const MultiInput = ({ onSubmit, fullWidth = true, variant = undefined,
     textMaxInput = 'Max number inputs', valuesInputs = [], color = undefined, direction = "column", maxInputs = Infinity,
-    iconMore = "+", iconLess = "-", label = "Input", btnAddStyle = {}, btnRemoveStyle = {}, helperText = '', isRequired = false, id = "", name = "", error = false ,textInputRequired="This input is required" }: MultiInputProps) => {
+    iconMore = "+", iconLess = "-", label = "Input", btnAddStyle = {}, btnRemoveStyle = {}, helperText = '', isRequired = false, id = "", name = "", error = false ,textInputRequired="This input is required",inputStyle={} }: MultiInputProps) => {
 
     const [inputs, setInputs] = useState<Item[]>([{ valueText: '', disabled: false }]);
     const [inputValue, setInput] = useState('');
@@ -85,7 +86,7 @@ export const MultiInput = ({ onSubmit, fullWidth = true, variant = undefined,
                 isEdit = true
                 //console.log(isEdit,'step2')
                 if (isEdit) {
-                    console.log('step3')
+                    //console.log('step3')
                     const newInputs = [...inputs];
                     valuesInputs.forEach(element => {
                         newInputs.push({ valueText: element, disabled: true })
@@ -95,7 +96,7 @@ export const MultiInput = ({ onSubmit, fullWidth = true, variant = undefined,
                     isEdit = false
                 }
             }
-            console.log('fuera')
+            //console.log('fuera')
         }
     }
     const maxInputsValidation = () => {
@@ -134,6 +135,7 @@ export const MultiInput = ({ onSubmit, fullWidth = true, variant = undefined,
                                 fullWidth={fullWidth}
                                 variant={variant}
                                 color={color}
+                                sx={inputStyle}
                             /> <Button sx={{ minWidth: "20px", borderRadius: "50%", m: 2, ...btnAddStyle }} variant="contained"
                                 color="primary" onClick={handleAddInput} disabled={action == 'maxInput' ? true : false} >
                                     {iconMore}
